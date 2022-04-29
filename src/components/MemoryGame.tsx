ac0983d6bb5ff2ac7082f6ca31d02d8c79937c35
nacen <nacen.dev@gmail.com>
2022-04-30 02:57:39 +0800

    Remove MemoryGameState and separate the states

--- src/components/MemoryGame.tsx
+++ src/components/MemoryGame.tsx
@@ -5,22 +5,16 @@ import { IScoreBoard } from "./Scoreboard";
 
 interface Props {}
 
-interface MemoryGameState {
-  cards: ICard[];
-  scores: IScoreBoard;
-}
-
 const MemoryGame = (props: Props) => {
-  const [memoryState, setMemoryState] = useState<MemoryGameState>({
-    cards: cardsData,
-    scores: {
-      currentScore: 0,
-      bestScore: 0,
-    },
+  const [cards, setCards] = useState<ICard[]>(cardsData);
+
+  const [scores, setScores] = useState<IScoreBoard>({
+    currentScore: 0,
+    bestScore: 0,
   });
 
   const shuffleCards = () => {
-    const tempCards = [...memoryState.cards];
+    const tempCards = [...cards];
     for (let i = tempCards.length - 1; i > 0; i--) {
       let randomIndex = Math.floor(Math.random() * (i + 1));
       [tempCards[i], tempCards[randomIndex]] = [
@@ -29,18 +23,16 @@ const MemoryGame = (props: Props) => {
       ];
     }
 
-    setMemoryState((prevState: MemoryGameState) => ({
-      ...prevState,
-      cards: tempCards
-    }));
+    setCards(tempCards);
   };
 
   const cardClicked = (selected: boolean) => {
+    shuffleCards();
   };
 
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 p-4 bg-slate-700">
-      {memoryState.cards.map((card) => (
+      {cards.map((card) => (
         <Card
           imgURL={card.imgURL}
           text={card.text}
