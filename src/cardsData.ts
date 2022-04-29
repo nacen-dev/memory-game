c025cd7b906ba0ab1f1258ee445fe9a08ff88dee
nacen <nacen.dev@gmail.com>
2022-04-30 02:37:46 +0800

    Create cardsData and add assets

---
+++ src/cardsData.ts
@@ -0,0 +1,112 @@
+import { ICard } from "./components/Card";
+import Blastoise from "./assets/240px-009Blastoise.png";
+import Venusaur from "./assets/240px-003Venusaur.png";
+import Arcanine from "./assets/240px-059Arcanine.webp";
+import Gengar from "./assets/240px-094Gengar.webp";
+import Snorlax from "./assets/240px-143Snorlax.webp";
+import Heracross from "./assets/240px-214Heracross.png";
+import Tyranitar from "./assets/240px-248Tyranitar.webp";
+import Gardevoir from "./assets/240px-282Gardevoir.webp";
+import Seviper from "./assets/240px-336Seviper.png";
+import Metagross from "./assets/240px-376Metagross.png";
+import Rayquaza from "./assets/240px-384Rayquaza.png";
+import Weavile from "./assets/240px-461Weavile.png";
+import Mamoswine from "./assets/240px-473Mamoswine.webp";
+import Eelektross from "./assets/240px-604Eelektross.png";
+import Palossand from "./assets/240px-770Palossand.png";
+import Corviknight from "./assets/240px-823Corviknight.png";
+import Sirfetchd from "./assets/240px-865Sirfetchd.png";
+import Zacian from "./assets/240px-888Zacian.png";
+
+export const cardsData: ICard[] = [
+  {
+    text: "Blastoise",
+    imgURL: Blastoise,
+    clicked: false,
+  },
+  {
+    text: "Venusaur",
+    imgURL: Venusaur,
+    clicked: false,
+  },
+  {
+    text: "Arcanine",
+    imgURL: Arcanine,
+    clicked: false,
+  },
+  {
+    text: "Gengar",
+    imgURL: Gengar,
+    clicked: false,
+  },
+  {
+    text: "Snorlax",
+    imgURL: Snorlax,
+    clicked: false,
+  },
+  {
+    text: "Heracross",
+    imgURL: Heracross,
+    clicked: false,
+  },
+  {
+    text: "Tyranitar",
+    imgURL: Tyranitar,
+    clicked: false,
+  },
+  {
+    text: "Gardevoir",
+    imgURL: Gardevoir,
+    clicked: false,
+  },
+  {
+    text: "Seviper",
+    imgURL: Seviper,
+    clicked: false,
+  },
+  {
+    text: "Metagross",
+    imgURL: Metagross,
+    clicked: false,
+  },
+  {
+    text: "Rayquaza",
+    imgURL: Rayquaza,
+    clicked: false,
+  },
+  {
+    text: "Weavile",
+    imgURL: Weavile,
+    clicked: false,
+  },
+  {
+    text: "Mamoswine",
+    imgURL: Mamoswine,
+    clicked: false,
+  },
+  {
+    text: "Eelektross",
+    imgURL: Eelektross,
+    clicked: false,
+  },
+  {
+    text: "Corviknight",
+    imgURL: Corviknight,
+    clicked: false,
+  },
+  {
+    text: "Palossand",
+    imgURL: Palossand,
+    clicked: false,
+  },
+  {
+    text: "Sirfetchd",
+    imgURL: Sirfetchd,
+    clicked: false,
+  },
+  {
+    text: "Zacian",
+    imgURL: Zacian,
+    clicked: false,
+  },
+];
