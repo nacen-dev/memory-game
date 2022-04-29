9ece30cb5418b4bfc211125d7538ad2c70c5355d
nacen <nacen.dev@gmail.com>
2022-04-30 02:41:37 +0800

    Use MemoryGame in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Header } from './components/Header';
+import { MemoryGame } from './components/MemoryGame';
 
 function App() {
   return (
     <div>
       <Header />
+      <MemoryGame />
     </div>
   );
 }
